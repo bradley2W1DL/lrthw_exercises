fb03a117de960f711e4a1ccc571eb9796c806ca3
Bradley Berger <hippy.berger@gmail.com>
2015-06-03 13:12:10 -0600

    Uploading readme file and ex1.rb
    [#95933996]

---
+++ chapter_1/ex1.rb
@@ -0,0 +1,23 @@
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'Print out another line, just for fun!'
+
+# Make your script print out another line (line 8 above)
+
+# Make your script print out just one line
+# -> for this I guess you could just comment out all but one line. Or does the
+# exercise want me to just repeat on of the above statements?
+puts # empty line for spacing
+puts '___________'
+puts 'Hello World!'
+puts # empty line for spacing
+
+# <- explain the almighty octothorpe hash-tag awesome
+# the octothorpe denotes a comment, ie. text that ruby will just skip over and
+# not try to run as code.  This is used to leave notes for yourself and other
+# programmers to better understand what they/you're looking at.
